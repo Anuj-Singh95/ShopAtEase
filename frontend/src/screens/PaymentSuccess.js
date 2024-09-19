43d69abfb951c789fbe214a97536c4978ee9d892
Anuj Singh <anujsingh92588@gmail.com>
2024-09-19 21:53:54 +0530

    api update

--- frontend/src/screens/PaymentSuccess.js
+++ frontend/src/screens/PaymentSuccess.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import { useDispatchCart } from '../components/ContextReducer';
+import React from "react";
+import { useDispatchCart } from "../components/ContextReducer";
 
 export default function PaymentSuccess() {
   const dispatch = useDispatchCart();
-   dispatch({type:"REMOVE"});
-    localStorage.removeItem("cart");
+  dispatch({ type: "REMOVE" });
+  localStorage.removeItem("cart");
   return (
     <div>
-        <h1>Order Successfully Placed</h1>
+      <h1>Order Successfully Placed</h1>
     </div>
-  )
+  );
 }
