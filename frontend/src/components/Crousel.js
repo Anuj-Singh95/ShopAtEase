a04e3002ccf39c8c289630e41d3cab94290500e4
Anuj Singh <anujsingh92588@gmail.com>
2024-09-19 23:13:22 +0530

    crousel update

--- frontend/src/components/Crousel.js
+++ frontend/src/components/Crousel.js
@@ -1,4 +1,7 @@
 import React from "react";
+import banner1 from "./../images/slider-imgs/banner1.jpg";
+import banner2 from "./../images/slider-imgs/banner2.jpg";
+import banner3 from "./../images/slider-imgs/banner3.jpg";
 
 export default function Crousel(props) {
   // const [searchKey, setSearchKey]= useState('');
@@ -19,25 +22,13 @@ export default function Crousel(props) {
     >
       <div className="carousel-inner" id="carousel">
         <div className="carousel-item active">
-          <img
-            src="https://shopiclix.com/adminpanel/upload/slider/1679574169494.png"
-            className="d-block w-100"
-            alt="..."
-          />
+          <img src={banner1} className="d-block w-100" alt="..." />
         </div>
         <div className="carousel-item">
-          <img
-            src="https://images.unsplash.com/photo-1542838132-92c53300491e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1074&q=80"
-            className="d-block w-100"
-            alt="..."
-          />
+          <img src={banner2} className="d-block w-100" alt="..." />
         </div>
         <div className="carousel-item">
-          <img
-            src="https://source.unsplash.com/random/900x700?grocery"
-            className="d-block w-100"
-            alt="..."
-          />
+          <img src={banner3} className="d-block w-100" alt="..." />
         </div>
       </div>
       <button
